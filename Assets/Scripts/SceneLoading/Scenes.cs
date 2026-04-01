7e692945820a230236a5dfb0991c7f34b5225381
lasagna <ssmrlll@yandex.ru>
2026-04-01 20:53:59 +0300

    addresables + SceneLoading

---
+++ Assets/Scripts/SceneLoading/Scenes.cs
@@ -0,0 +1,9 @@
+﻿namespace SceneLoading
+{
+    public static class Scenes
+    {
+        public const string BOOT = "Boot";
+        public const string MENU = "Menu";
+        public const string GAME = "Game";
+    }
+}
