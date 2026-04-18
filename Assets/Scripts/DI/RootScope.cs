eaaadf17aa83c2405436731b558e6592486491fc
lasagna <ssmrlll@yandex.ru>
2026-04-18 11:15:29 +0300

    start game + gamedata

--- Assets/Scripts/DI/RootScope.cs
+++ Assets/Scripts/DI/RootScope.cs
@@ -3,6 +3,7 @@ using Boot;
 using SceneLoading;
 using UnityEngine;
 using Audio;
+using Data;
 using VContainer;
 using VContainer.Unity;
 
@@ -16,6 +17,7 @@ namespace DI
         protected override void Configure(IContainerBuilder builder)
         {
             builder.RegisterEntryPoint<BootEntryPoint>();//не спрашивает лайфтайм,так как это регистрация точки входа
+            builder.Register<GameData>(Lifetime.Singleton);
             builder.Register<IAsyncSceneLoading, AsyncSceneLoading>(Lifetime.Singleton);
             builder.Register<IAnimation, AnimationManager>(Lifetime.Singleton);//регистрирцем здесь так как анимации будут и в руте в том числе а не только в игре
             builder.RegisterInstance(_loadingView); 
